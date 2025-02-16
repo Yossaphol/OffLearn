cba4f1c0856d639f34e5d92556f02159eea02f0a
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-02-16 14:56:15 +0700

    Update Login/Sign up GUI and Add Forgot password form.

--- src/main/java/loginAndSignUp/loginController.java
+++ src/main/java/loginAndSignUp/loginController.java
@@ -1,13 +1,126 @@
 package loginAndSignUp;
 
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class loginController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    // Link to signup
+    @FXML
+    private void switchtoSignUp(ActionEvent event) throws IOException {
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5));
+        fadeOut.setNode(((Node) event.getSource()).getScene().getRoot());
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+
+        fadeOut.setOnFinished(e -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/signup.fxml"));
+                root = loader.load();
+
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+
+
+                FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.5));
+                fadeIn.setNode(root);
+                fadeIn.setFromValue(0);
+                fadeIn.setToValue(1);
+
+                fadeIn.play();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        fadeOut.play();
+    }
+
+    // Link to forgotPassword
+    @FXML
+    private void forgotPassword(ActionEvent event) throws IOException {
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5));
+        fadeOut.setNode(((Node) event.getSource()).getScene().getRoot());
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+
+        fadeOut.setOnFinished(e -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/forgotPassword.fxml"));
+                root = loader.load();
+
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+
+
+                FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.5));
+                fadeIn.setNode(root);
+                fadeIn.setFromValue(0);
+                fadeIn.setToValue(1);
+
+                fadeIn.play();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        fadeOut.play();
+    }
+
+    // Link to login
+    @FXML
+    public void toLogin(ActionEvent event) throws IOException {
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5));
+        fadeOut.setNode(((Node) event.getSource()).getScene().getRoot());
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+
+        fadeOut.setOnFinished(e -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+                root = loader.load();
+
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+
+
+                FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.5));
+                fadeIn.setNode(root);
+                fadeIn.setFromValue(0);
+                fadeIn.setToValue(1);
+
+                fadeIn.play();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        fadeOut.play();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
