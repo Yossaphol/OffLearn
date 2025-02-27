ee0b677c308ebd530837c83f967fe4c9a870d819
Alakok77 <top.p1234558@gmail.com>
2025-02-27 18:53:06 +0700

    add payment display

---
+++ src/main/java/client/payment/payment.java
@@ -0,0 +1,18 @@
+package client.payment;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class payment extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(payment.class.getResource("/fxml/client/payment/payment.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 650);
+
+        stage.setTitle("Offlearn Client");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
