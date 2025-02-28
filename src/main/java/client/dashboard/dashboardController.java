d46bd9751f510f776b86bc0f03dc5241d4eb4ccf
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-02-28 22:09:07 +0700

    Update Dashboard GUI -Update chart data -Add hover effect -Add elements

--- src/main/java/client/dashboard/dashboardController.java
+++ src/main/java/client/dashboard/dashboardController.java
@@ -2,31 +2,32 @@ package client.dashboard;
 
 import client.FontLoader.FontLoader;
 import javafx.animation.FadeTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import client.HomeAndNavigation.Navigator;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
 import client.HomeAndNavigation.HomeController;
 import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class dashboardController implements Initializable {
@@ -67,8 +68,40 @@ public class dashboardController implements Initializable {
     public ProgressBar roadmap_progress1;
     public Label roadmap_value;
     public Label roadmap_value1;
+    public Pane cat1;
+    public Pane cat2;
+    public Pane user_profile;
+    public VBox quickInbox;
+    public VBox quizBox;
+    public VBox roadmapProgression;
+    public VBox courseProgression;
+    public VBox scoreTendency;
+    public VBox risk;
+    public VBox studyTable;
+    public Button btn_continue;
+    public Button btn_otherCourse;
+    public Label progressCategory;
+    public Label progressCategory1;
+    public ProgressBar categorybar1;
+    public ProgressBar categorybar;
+    public Label first_val;
+    public Label first_subject_name;
+    public Label second_val;
+    public Label second_subject_name;
+    public Label third_val;
+    public Label third_subject_name;
+    public BarChart scoreChart;
+    public CategoryAxis xAxis_score;
+    public NumberAxis yAxis_score;
 
+    @FXML
+    private StackedBarChart<String, Number> courseProgressionChart;
 
+    @FXML
+    private CategoryAxis xAxis;
+
+    @FXML
+    private NumberAxis yAxis;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         FontLoader fontLoader = new FontLoader();
@@ -97,6 +130,21 @@ public class dashboardController implements Initializable {
         method_home.hoverEffect(home);
         method_home.hoverEffect(yourCoursebtn);
         method_home.hoverEffect(calendarContainer);
+        method_home.hoverEffect(allCoursebtn);
+        method_home.hoverEffect(cat1);
+        method_home.hoverEffect(cat2);
+        method_home.hoverEffect(user_profile);
+        method_home.hoverEffect(quickInbox);
+        method_home.hoverEffect(quizBox);
+        method_home.hoverEffect(roadmapProgression);
+        method_home.hoverEffect(courseProgression);
+        method_home.hoverEffect(scoreTendency);
+        method_home.hoverEffect(risk);
+        method_home.hoverEffect(studyTable);
+        method_home.hoverEffect(btn_continue);
+        method_home.hoverEffect(btn_otherCourse);
+
+        method_home.applyHoverEffectToInside(quickInbox);
 
         route();
 
@@ -115,9 +163,14 @@ public class dashboardController implements Initializable {
 
         us_st.setViewOrder(-1);
 
-
+        categorybar.setProgress(Double.parseDouble(progressCategory.getText().replace("% completed", "").trim()) / 100);
+        categorybar1.setProgress(Double.parseDouble(progressCategory1.getText().replace("% completed", "").trim()) / 100);
         roadmap_progress.setProgress(Double.parseDouble(roadmap_value.getText().replace("%", "").trim()) / 100);
         roadmap_progress1.setProgress(Double.parseDouble(roadmap_value1.getText().replace("%", "").trim()) / 100);
+
+        //Call chart
+        courseProgressionChart();
+        scoreChart();
     }
 
 
@@ -234,6 +287,65 @@ public class dashboardController implements Initializable {
         });
         categoryBox.setOnMouseExited(event -> {
             categoryBox.setStyle("-fx-background-color: transparent;");
+        });
+    }
+
+
+    private void courseProgressionChart() {
+        if (xAxis == null || yAxis == null || courseProgressionChart == null) {
+            System.out.println("FXML components not setup properly!");
+            return;
+        }
+
+        XYChart.Series<String, Number> module1 = new XYChart.Series<>();
+        module1.setName(first_subject_name.getText());
+        module1.getData().add(new XYChart.Data<>("Overall course progression (ร้อยละ %)", Double.parseDouble(first_val.getText().replace("%", "")) / 100));
+
+        XYChart.Series<String, Number> module2 = new XYChart.Series<>();
+        module2.setName(second_subject_name.getText());
+        module2.getData().add(new XYChart.Data<>("Overall course progression (ร้อยละ %)", Double.parseDouble(second_val.getText().replace("%", "")) / 100));
+
+        XYChart.Series<String, Number> module3 = new XYChart.Series<>();
+        module3.setName(third_subject_name.getText());
+        module3.getData().add(new XYChart.Data<>("Overall course progression (ร้อยละ %)", Double.parseDouble(third_val.getText().replace("%", "")) / 100));
+
+        courseProgressionChart.getData().addAll(module1, module2, module3);
+
+        Platform.runLater(() -> {
+            courseProgressionChart.lookup(".default-color2.chart-bar").setStyle("-fx-bar-fill: " +getColorLabel(first_val)+ ";"+"-fx-background-radius: 0;");
+            courseProgressionChart.lookup(".default-color1.chart-bar").setStyle("-fx-bar-fill: " +getColorLabel(second_val)+ ";"+"-fx-background-radius: 0;");
+            courseProgressionChart.lookup(".default-color0.chart-bar").setStyle("-fx-bar-fill: " +getColorLabel(third_val)+ ";"+"-fx-background-radius: 0;");
+        });
+    }
+
+    public String getColorLabel(Label label) {
+        Color labelColor = (Color) label.getTextFill();
+        return String.format("#%02X%02X%02X",
+                (int) (labelColor.getRed() * 255),
+                (int) (labelColor.getGreen() * 255),
+                (int) (labelColor.getBlue() * 255));
+    }
+
+
+    private void scoreChart() {
+        XYChart.Series<String, Number> module1 = new XYChart.Series<>();
+        module1.setName(first_subject_name.getText());
+        module1.getData().add(new XYChart.Data<>("Score Overview", 13000.4));
+
+        XYChart.Series<String, Number> module2 = new XYChart.Series<>();
+        module2.setName(second_subject_name.getText());
+        module2.getData().add(new XYChart.Data<>("Score Overview",27000.83));
+
+        XYChart.Series<String, Number> module3 = new XYChart.Series<>();
+        module3.setName(third_subject_name.getText());
+        module3.getData().add(new XYChart.Data<>("Score Overview", 9700));
+
+        scoreChart.getData().addAll(module1, module2, module3);
+
+        Platform.runLater(() -> {
+            scoreChart.lookup(".default-color2.chart-bar").setStyle("-fx-bar-fill: " +getColorLabel(first_val)+ ";"+"-fx-background-radius: 0;");
+            scoreChart.lookup(".default-color1.chart-bar").setStyle("-fx-bar-fill: " +getColorLabel(second_val)+ ";"+"-fx-background-radius: 0;");
+            scoreChart.lookup(".default-color0.chart-bar").setStyle("-fx-bar-fill: " +getColorLabel(third_val)+ ";"+"-fx-background-radius: 0;");
         });
     }
 
