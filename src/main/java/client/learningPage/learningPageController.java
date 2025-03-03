e8557a42e7da7d6909538b7fd13a04f7be81ce9d
LCUKP <puwanatjaijitjaam@gmail.com>
2025-03-03 11:30:48 +0700

    Add NavBar and SearchBar Template

--- src/main/java/client/learningPage/learningPageController.java
+++ src/main/java/client/learningPage/learningPageController.java
@@ -1,4 +1,53 @@
 package client.learningPage;
 
-public class learningPageController {
+import client.FontLoader.FontLoader;
+import client.HomeAndNavigation.HomeController;
+import client.HomeAndNavigation.Navigator;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class learningPageController implements Initializable {
+    public VBox leftWrapper;
+    public HBox rootpage;
+    public HBox searhbar_container;
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FontLoader fontLoader = new FontLoader();
+        fontLoader.loadFonts();
+        
+        //HomeController method_home = new HomeController();
+
+        displayNavbar();
+        displaySearchBar();
+    }
+
+
+
+
+    private void displayNavbar(){
+        try {
+            FXMLLoader calendarLoader = new FXMLLoader(getClass().getResource("/fxml/client/NavAndSearchbar/navBar.fxml"));
+            VBox navContent = calendarLoader.load();
+            leftWrapper.getChildren().setAll(navContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displaySearchBar(){
+        try {
+            FXMLLoader calendarLoader = new FXMLLoader(getClass().getResource("/fxml/client/NavAndSearchbar/searchBar.fxml"));
+            HBox searchbarContent = calendarLoader.load();
+            searhbar_container.getChildren().setAll(searchbarContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
