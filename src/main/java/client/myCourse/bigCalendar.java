3d229329c38f057763cd8f227802ec6de19947e8
BWorathit275 <67070275@kmitl.ac.th>
2025-03-04 22:23:53 +0700

    add Mycourse Calendar
    -fxml
    -controller
    -app

---
+++ src/main/java/client/myCourse/bigCalendar.java
@@ -0,0 +1,27 @@
+package client.myCourse;
+
+import client.HomeAndNavigation.Home;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class bigCalendar extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Home.class.getResource("/fxml/client/courseManage/customBigCalendar.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setTitle("Offlearn Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
