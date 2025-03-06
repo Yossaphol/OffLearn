6e4a842dc9d81873c5b516e966fb29c14ffbc9fd
Alakok77 <top.p1234558@gmail.com>
2025-03-06 14:43:49 +0700

    add user database

---
+++ src/main/java/Database/UserDB.java
@@ -0,0 +1,30 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDB extends ConnectDB{
+
+    public void testConnect(){
+        String query = "SELECT StudentID, Fullname FROM studentdb.user";
+
+        try (Connection conn = this.connectToDB();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                int studentId = rs.getInt("StudentID");
+                String fullName = rs.getString("Fullname");
+
+                System.out.println("ID: " + studentId + ", Name: " + fullName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
---
+++ src/main/java/Database/test.java
@@ -0,0 +1,10 @@
+package Database;
+
+public class test {
+    static UserDB con = new UserDB();
+
+    public static void main(String[] args) {
+        con.testConnect();
+    }
+
+}
