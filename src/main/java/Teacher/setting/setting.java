ead371fd29d7f9cc00194546692660d16c9a1ca2
Thithat240 <67070240@kmitl.ac.th>
2025-03-08 19:48:38 +0700

    teacher setting gui

---
+++ src/main/java/Teacher/setting/setting.java
@@ -0,0 +1,28 @@
+package Teacher.setting;
+
+import Student.roadmap.myroadmap;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class setting extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(myroadmap.class.getResource("/fxml/Student/courseManage/myroadmap.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        primaryStage.setTitle("Offlearn");
+        primaryStage.setMaximized(true);
+        primaryStage.setFullScreen(true);
+        primaryStage.setFullScreenExitHint("");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
