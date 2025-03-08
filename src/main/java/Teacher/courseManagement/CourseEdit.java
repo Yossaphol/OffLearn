08a1891751a9df17b21aaf3cdc1aad00a774dfaf
Puriwaj Pirasakulpat <67070138@kmitl.ac.th>
2025-03-08 23:52:11 +0700

    Add courseEdit GUI and controller (not finished)

---
+++ src/main/java/Teacher/courseManagement/CourseEdit.java
@@ -0,0 +1,27 @@
+package Teacher.courseManagement;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CourseEdit extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Teacher.courseManagement.CourseEditController.class.getResource("/fxml/Teacher/courseManage/courseEdit.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        primaryStage.setTitle("Offlearn");
+        primaryStage.setMaximized(true);
+        primaryStage.setFullScreen(true);
+        primaryStage.setFullScreenExitHint("");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
