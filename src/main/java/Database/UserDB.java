a9716d59c66eff20afd15f8239870986d8491a89
Yossaphol <yossaphol3502@gmail.com>
2025-03-10 15:33:56 +0700

    add login, register and update password system

--- src/main/java/Database/UserDB.java
+++ src/main/java/Database/UserDB.java
@@ -7,24 +7,121 @@ import java.sql.SQLException;
 
 public class UserDB extends ConnectDB{
 
-    public void testConnect(){
-        String query = "SELECT User_ID, Fullname FROM studentdb.user";
+
+    public boolean loginConnect(String username, String password){
+        String query = "SELECT Username, Password FROM studentdb.user WHERE Username = ? AND Password = ?";
 
         try (Connection conn = this.connectToDB();
              PreparedStatement pstmt = conn.prepareStatement(query)) {
 
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
             ResultSet rs = pstmt.executeQuery();
 
-            while (rs.next()) {
-                int studentId = rs.getInt("User_ID");
-                String fullName = rs.getString("Fullname");
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-                System.out.println("ID: " + studentId + ", Name: " + fullName);
+    public boolean signupConnect(String fullname, String username, String password, String email) {
+        String query = "INSERT INTO studentdb.user (Fullname, Username, Password, Email) VALUES (?, ?, ?, ?)";
+
+        if (isUsernameExist(username)) {
+            System.out.println("This username is already registered. please use another username to register.");
+            return false;
+        }
+        if (isEmailExist(email)) {
+            System.out.println("The email is already registered. Please use another email address to register.");
+            return false;
+        }
+
+        try (Connection conn = this.connectToDB();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, fullname);
+            pstmt.setString(2, username);
+            pstmt.setString(3, password);
+            pstmt.setString(4, email);
+
+            int affectedRows = pstmt.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    private boolean isUsernameExist(String username) {
+        String query = "SELECT COUNT(*) FROM studentdb.user WHERE Username = ?";
+
+        try (Connection conn = this.connectToDB();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, username);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return false;
+    }
+    private boolean isEmailExist(String email) {
+        String query = "SELECT COUNT(*) FROM studentdb.user WHERE Email = ?";
+
+        try (Connection conn = this.connectToDB();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, email);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
+    public boolean isUserValid(String username, String email) {
+        String query = "SELECT COUNT(*) FROM studentdb.user WHERE Username = ? AND Email = ?";
+
+        try (Connection conn = this.connectToDB();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, email);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updatePasswordConnect(String email, String username, String newPassword) {
+        String query = "UPDATE studentdb.user SET Password = ? WHERE Username = ? AND Email = ?";
+
+        try (Connection conn = this.connectToDB();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, newPassword);
+            pstmt.setString(2, username);
+            pstmt.setString(3, email);
+            int affectedRows = pstmt.executeUpdate();
+
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
 
--- src/main/java/Database/test.java
+++ src/main/java/Database/test.java
@@ -2,9 +2,7 @@ package Database;
 
 public class test {
     static QuestionDB con = new QuestionDB();
-
     public static void main(String[] args) {
         con.saveQuestion("test", "test", 5);
     }
-
 }
