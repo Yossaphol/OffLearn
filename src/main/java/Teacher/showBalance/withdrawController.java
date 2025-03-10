a2dcd4086276b5c9f8ed2d33f74becac6d7c0caf
Yossaphol <yossaphol3502@gmail.com>
2025-03-10 15:32:55 +0700

    edit withdraw and fixed Home.css

--- src/main/java/Teacher/showBalance/withdrawController.java
+++ src/main/java/Teacher/showBalance/withdrawController.java
@@ -1,13 +1,29 @@
 package Teacher.showBalance;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class withdrawController implements Initializable {
+
+    @FXML
+    public HBox navbarContainer;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        displayNavbar();
+    }
+    public void displayNavbar(){
+        try {
+            FXMLLoader calendarLoader = new FXMLLoader(getClass().getResource("/fxml/Teacher/navBar/navBar.fxml"));
+            HBox navContent = calendarLoader.load();
+            navbarContainer.getChildren().setAll(navContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
