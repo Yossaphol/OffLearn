f4c08c7f2a623742679379fe087d9a4e913192da
Puriwaj Pirasakulpat <67070138@kmitl.ac.th>
2025-03-10 18:37:36 +0700

    Update roadmap - add fxid

--- src/main/java/Student/roadmap/roadmapController.java
+++ src/main/java/Student/roadmap/roadmapController.java
@@ -3,6 +3,8 @@ package Student.roadmap;
 import Student.FontLoader.FontLoader;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -14,6 +16,25 @@ public class roadmapController implements Initializable {
     public HBox MainFrame;
     public VBox LeftWrapper;
     public HBox searhbar_container;
+    public Button yourRoadmap;
+    public Label category01;
+    public Label category02;
+    public Label category03;
+    public Label category04;
+    public Label category05;
+    public Label category06;
+    public Label category07;
+    public Label category08;
+    public Button sub_category_btn01;
+    public Button sub_category_btn02;
+    public Button sub_category_btn03;
+    public Button sub_category_btn04;
+    public Button sub_category_btn05;
+    public Button sub_category_btn06;
+    public Button sub_category_btn07;
+    public Button sub_category_btn08;
+    public Button sub_category_btn09;
+    public Button sub_category_btn10;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
