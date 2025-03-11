1391b9d15385dd73eabc5e8d0d8295a1ad92b057
Serried <contact.rewmb@gmail.com>
2025-03-11 18:17:40 +0700

    feat(student): add courseEnrollController.java

---
+++ src/main/java/Student/courseManage/courseEnrollController.java
@@ -0,0 +1,23 @@
+package Student.courseManage;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class courseEnrollController implements Initializable {
+    @FXML
+    public VBox leftWrapper;
+    public HBox rootPage;
+    public HBox searchbar_container;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
