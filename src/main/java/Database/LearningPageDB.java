605dfeff11d897694eba36ac1a705a6a389f2bf3
BWorathit275 <67070275@kmitl.ac.th>
2025-03-28 14:51:45 +0700

    LearningPage Database prepping
    learnpageDB for playlist
    ProgressDB for progress bar
    VideoPathDB for Video data (unsure)
    ChapterDB for "Episode"
    QuizDB for Quiz "Episode"
    UserID for uhhh course teacher i guess
    IDK ABOUT COMMENTS
    and attachment idk which db it use, chapter?
    
    stuff inside learnpage and progress db is not ready for commit yet
    
    also ffpmeg still need adjust it load soo slow

---
+++ src/main/java/Database/LearningPageDB.java
@@ -0,0 +1,12 @@
+package Database;
+
+import Student.learningPage.PlaylistItem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class LearningPageDB extends ConnectDB {
+
+}
