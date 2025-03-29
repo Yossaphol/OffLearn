08dc29e50d7e77df0794a40a04eba0a89ab909c2
Yossaphol <yossaphol3502@gmail.com>
2025-03-30 01:21:12 +0700

    update withdraw and withdraw details

--- src/main/java/Teacher/showBalance/withdrawController.java
+++ src/main/java/Teacher/showBalance/withdrawController.java
@@ -1,11 +1,17 @@
 package Teacher.showBalance;
 
+import Database.EnrollDB;
+import Database.UserDB;
+import Database.withdraw;
+import Database.withdrawDB;
 import Student.HomeAndNavigation.HomeController;
 import Teacher.navigator.Navigator;
+import a_Session.SessionManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 import java.io.IOException;
@@ -19,6 +25,12 @@ public class withdrawController implements Initializable {
     public Button withdrawBtn;
     public Button edit;
     public Button withdrawHistory;
+    public Label accountNo;
+    public Label accountName;
+    public Label bankname;
+    public Label totalMoney;
+
+    EnrollDB enrollDB = new EnrollDB();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -28,6 +40,22 @@ public class withdrawController implements Initializable {
         setEffect();
         route();
 
+        showacct();
+
+    }
+
+    private void showacct() {
+        String sessionUsername = SessionManager.getInstance().getUsername();
+        UserDB userDB = new UserDB();
+        int userId = userDB.getUserId(sessionUsername);
+        withdraw userWithdraw = withdrawDB.getWithdrawInfo((userId));
+        String acctname = userWithdraw.getAccountFName() + " " + userWithdraw.getAccountLName();
+        accountNo.setText(userWithdraw.getAccountNumber());
+        accountName.setText(acctname);
+        bankname.setText(userWithdraw.getBankName());
+
+        double totalCost = enrollDB.getTotalEnrollments(userId);
+        totalMoney.setText(String.format("%.2f", totalCost));
     }
 
     private void route(){
