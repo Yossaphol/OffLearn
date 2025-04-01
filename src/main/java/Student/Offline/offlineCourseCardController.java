87bd97f869889d5a890696780cf0bb2d5b544386
BWorathit275 <67070275@kmitl.ac.th>
2025-04-01 19:07:21 +0700

    design adjust for offline stuff
    not sure if i wanna make profer pull course img

--- src/main/java/Student/Offline/offlineCourseCardController.java
+++ src/main/java/Student/Offline/offlineCourseCardController.java
@@ -4,8 +4,10 @@ import Utili.OfflineCourseData;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 public class offlineCourseCardController {
@@ -13,6 +15,7 @@ public class offlineCourseCardController {
     @FXML private Label courseTitle;
     @FXML private Label courseSubtitle;
     @FXML private HBox courseCard;
+    @FXML private Rectangle courseImage;
 
     private OfflineCourseData data;
 
@@ -21,6 +24,10 @@ public class offlineCourseCardController {
         this.courseTitle.setText(data.getCourseName());
         this.courseCategory.setText(data.getCourseCategory());
         this.courseSubtitle.setText(data.getCourseDescription());
+        String imgPath = getRandomOfflineImage();
+        Image image = new Image(getClass().getResourceAsStream(imgPath));
+        ImagePattern pattern = new ImagePattern(image);
+        courseImage.setFill(pattern);
 
     }
 
@@ -31,4 +38,18 @@ public class offlineCourseCardController {
     public OfflineCourseData getCourseData() {
         return data;
     }
+
+    private static final String[] randomimg = {
+            "/img/Picture/a3ea0f6ea140c07de2d150b8e7e48826.jpg",
+            "/img/Picture/bg.jpg",
+            "/img/Picture/florian-olivo-4hbJ-eymZ1o-unsplash.jpg",
+            "/img/Picture/huh.jpg",
+            "default_course_pic"
+    };
+
+    private String getRandomOfflineImage() {
+        int index = (int) (Math.random() * randomimg.length);
+        return randomimg[index];
+    }
+
 }
